f174bd3b36bd81311bad3c98794282c3e1af5c11
modern coder <xien.thomas@gmail.com>
2021-10-07 14:21:39 -0500

    added bot with token

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import discord
+import os
+from dotenv import load_dotenv
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Hello {0.user}'.format(client))
+
+# client.run(os.getenv('TOKEN'))
 def main():
+    load_dotenv()
+    
+    client.run(os.getenv('TOKEN'))
     print("Hello World")
 
 
